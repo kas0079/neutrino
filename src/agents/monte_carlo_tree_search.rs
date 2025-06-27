5fd5ed6a8be88fb3174b568ed99449ef920f76b9
Kasper O <kaspokumu@gmail.com>
2025-06-27 22:48:47 +0200

    moved to indexed vec, tree repr

--- src/agents/monte_carlo_tree_search.rs
+++ src/agents/monte_carlo_tree_search.rs
@@ -1,62 +1,60 @@
 
 use std::{cell::{RefCell, RefMut}, collections::HashMap, rc::Weak};
 
-use graph::prelude::{DirectedALGraph, DirectedCsrGraph, DirectedNeighborsWithValues, NodeValues};
 use rand::{rng, seq::IteratorRandom};
 
 use crate::{agents::{agent::Agent, random_agent::RandomAgent}, neutrino_board::{GameBoard, TurnMove}};
 
+type NodeIndex = usize;
 #[derive(Clone)]
 struct NodeType {
     utility: f64,
     number_of_playouts: usize,
     board: GameBoard,
-    children: HashMap<TurnMove, RefCell<NodeType>>,
-    parent: RefCell<Weak<NodeType>>
+    children: HashMap<TurnMove, NodeIndex>,
+    parent: Option<NodeIndex>
 }
 
 impl NodeType {
-    fn new(board: GameBoard, parent: Option<Weak<NodeType>>) -> Self {    
-        Self { utility: 0., number_of_playouts: 0, board, children: HashMap::new(), parent: if parent.is_some() {RefCell::new(parent.unwrap())} else {RefCell::new(Weak::new())} }
+    fn new(board: GameBoard, parent: Option<NodeIndex>) -> Self {    
+        Self { utility: 0., number_of_playouts: 0, board, children: HashMap::new(), parent }
     }
     
     fn is_leaf(&self) -> bool {
         self.children.is_empty()
     }
 }
-pub(crate) struct MonteCarloTreeSearch  {  
-    root: RefCell<NodeType>
+pub(crate) struct MonteCarloTreeSearch  {
+    nodes: Vec<NodeType>,
+    root: usize
 }
 
 impl MonteCarloTreeSearch {
     pub(crate) fn new(board: &GameBoard) -> Self {
-        let root = RefCell::new(NodeType::new(board.clone(), None));
-        Self { root }
+        let root = NodeType::new(board.clone(), None);
+        Self { nodes: vec![root], root: 0 }
     }
     
-    fn select(&mut self) -> RefCell<NodeType> {
-        let mut node = self.root.clone();
+    fn select(&self) -> NodeIndex {
+        let mut node = self.root;
         let mut random_agent = RandomAgent::default();
-        while !node.borrow().is_leaf() {
-            let action: TurnMove;
-            let child:RefCell<NodeType>;
-            {
-                action = random_agent.get_move(&node.borrow().board);
-                child = node.borrow().children.get(&action).unwrap().clone();
-            }
-            node = child
+        while !self.nodes[node].is_leaf() {
+            let action: TurnMove = random_agent.get_move(&self.nodes[node].board);
+            let child = self.nodes[node].children.get(&action).unwrap();
+            node = *child
         }
         node
     }
     
-    fn expand(&mut self, node: &mut NodeType) {
-        for action in &node.board.actions() {
-            let new_board = node.board.result(action.clone());
-            let parent: Weak<NodeType> = Weak::from(value);
+    fn expand(&mut self, node: NodeIndex) {
+        let parent: NodeIndex = node;
+        for action in self.nodes[node].board.actions() {
+            let new_board = self.nodes[node].board.result(action.clone());
             let child = NodeType::new(new_board, Some(parent));
-            node.children.insert(action.clone(), child.into());
+            let nodeIndex = self.nodes.len();
+            self.nodes.push(child);
+            self.nodes[parent].children.insert(action, nodeIndex);
         }
-        todo!()
     }
     /*
     fn simulate() -> Self {
